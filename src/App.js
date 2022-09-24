251c57f964610594062da58328eb4bc2d02889da
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-09-24 16:55:15 +0700

    feat: functional to class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
-import links from './links.js';
+import data from './links.json';
 
-function App() {
-  const renderLinks = () => {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Adhy Wiranto",
+      links: data.links
+    }
+  }
+
+  renderLinks = () => {
     return (
-      links.map(link => {
+      this.state.links.map(link => {
         return (
           <div className='item'>
             <a href={link.url}><i className={link.icon}></i>{link.title}</a>
@@ -14,29 +23,33 @@ function App() {
     );
   }
 
-  return (
-    <div className='main'>
-      
-      <div className='contents'>
-        <div className="header">
-          <div className='myPhoto'>
-            <img src="/img/photo.jpg" alt="Adhy" />
+  render() {
+    return (
+      <div className='main'>
+
+        <div className='contents'>
+          <div className="header">
+            <div className='myPhoto'>
+              <img src="/img/photo.jpg" alt="Adhy" />
+            </div>
+            <div className='myTitle'>
+              <h1>{this.state.name}</h1>
+              <p>Welcome to my own <b>Linktree</b>!</p>
+            </div>
           </div>
-          <div className='myTitle'>
-            <h1>Adhy Wiranto</h1>
-            <p>Welcome to my own <b>Linktree</b>!</p>
+
+          <div className="links">
+            {this.renderLinks()}
           </div>
         </div>
-        
-        <div className="links">
-          {renderLinks()}
+
+        <div className='footer'>
+          <small><b>My Linked</b> &#169; {new Date().getFullYear()}. Made with &#10084; By <a className="link-dark" href="https://github.com/adhywiranto44" target="_blank" rel="noreferrer">Adhy Wiranto</a></small>
         </div>
+
       </div>
-      
-      <div className='footer'>
-        <small><b>My Linked</b> &#169; {new Date().getFullYear()}. Made with &#10084; By <a className="link-dark" href="https://github.com/adhywiranto44" target="_blank" rel="noreferrer">Adhy Wiranto</a></small>
-      </div>
-    </div>
-  );
+    )
+  }
 }
+
 export default App;
